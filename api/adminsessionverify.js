b8af4adb1b9d0472a07dbf318b358cc35b33c3c0
Volkdon <info@volkdonations.website>
2026-02-28 10:34:32 +0300

    Update adminsessionverify.js

--- api/adminsessionverify.js
+++ api/adminsessionverify.js
@@ -1,6 +1,6 @@
 const { neon } = require("@neondatabase/serverless");
 
-const DEBUG = false;
+const DEBUG = true;
 const log = (...args) => { if (DEBUG) console.log("[adminsessionverify]", ...args); };
 const err = (...args) => { if (DEBUG) console.error("[adminsessionverify]", ...args); };
 
